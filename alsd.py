2b7c6d62a159f3fc8cb443027d2270fee2a4a9ed
Tony McKendry <tonymckendry@gmail.com>
2025-03-17 16:20:45 -0600

    Update for Python3

--- alsd.py
+++ alsd.py
@@ -11,7 +11,9 @@ import plistlib
 
 # the maybe monad
 def bind(val, f):
-    return (val is not None) and f(val) or None
+    if val is None:
+        return None
+    return f(val)
 
 hexchars = set('0123456789abcdef')
 def decodeHexString(str):
@@ -33,7 +35,10 @@ def BoolValue(str): return  (str == "true")
 
 # return the type function for a string value
 def guessTypeForValue(v):
-    if (v in ('true','false')): return BoolValue
+    if v is None:
+        return None
+    if (v in ('true','false')): 
+        return BoolValue
     for fn in (int, float):
         try:
             if fn(v) is not None: 
@@ -55,7 +60,7 @@ class ALSNode(object):
     def __init__(self, elem):
         self.elem = elem
         # populate instance vars using harvested Value attributes from the element or subelements
-        for (key, fspec) in self.valuefields.iteritems():
+        for (key, fspec) in self.valuefields.items():
             if type(fspec) == dict:
                 (sel, ivar, vtype ) = (fspec.get('sel',key),fspec.get('ivar',key),fspec.get('type',None))
             elif type(fspec) == tuple:  # A simple (type, sel) tuple
@@ -183,13 +188,21 @@ class LiveSetTrackData(ALSNode):
     def __init__(self, elem):
         super(LiveSetTrackData, self).__init__(elem)
         self.trackType = elem.tag
-        self.devices = [LiveSetDeviceData(c) for c in elem.find("DeviceChain/DeviceChain/Devices")]
+        
+        # Handle device chain more safely
+        device_chain = elem.find("DeviceChain")
+        if device_chain is not None:
+            devices_elem = device_chain.find("DeviceChain/Devices")
+            self.devices = [LiveSetDeviceData(c) for c in devices_elem] if devices_elem is not None else []
+        else:
+            self.devices = []
 
         self.mixer = bind(elem.find("DeviceChain/Mixer"), ALSTrackMixer)
 
-        # TODO: encapsulate these in a class
-        self.clipslots = bind(elem.find("DeviceChain/MainSequencer/ClipSlotList"), lambda x:x.findall("ClipSlot")) or []
-        self.midiclips = bind(elem.find("DeviceChain/MainSequencer/ClipSlotList"), lambda x:[LiveSetMidiClipData(c) for c in x.findall(".//MidiClip")]) or []
+        # Handle clip slots more safely
+        clip_slot_list = bind(elem.find("DeviceChain/MainSequencer/ClipSlotList"), lambda x:x)
+        self.clipslots = clip_slot_list.findall("ClipSlot") if clip_slot_list is not None else []
+        self.midiclips = [LiveSetMidiClipData(c) for c in elem.findall(".//MidiClip")] if elem is not None else []
 
 
 class LiveSetData(object):
@@ -211,42 +224,48 @@ def dumpinfo(path, track=None, show_devices=True, show_clips=False, show_global=
     """Print out some info about an Ableton Live set at a path"""
     lsd = LiveSetData(path)
 
-    if show_global:
+    if show_global and lsd.mastertrack is not None and lsd.mastertrack.mixer is not None:
         globalitems = lsd.mastertrack.mixer.params.items()
         globalitems.sort(key=lambda i:i[0])
         for pk, pv in globalitems:
             if pk != "TimeSignature":
-                print "%s: "%pk, pv.manual    
+                print("%s: "%pk, pv.manual)    
         timesigs =  lsd.timeSignatures()
         if len(timesigs) == 1:
-            print "Time signature: %d/%d"%timesigs[0][1]
+            print("Time signature: %d/%d"%timesigs[0][1])
         else:
-            print "Time signatures: %s"%(", ".join(["%d/%d"%ts for (tm,ts) in timesigs ]))
-
+            print("Time signatures: %s"%(", ".join(["%d/%d"%ts for (tm,ts) in timesigs ])))
 
     def dumptrack(i, t):
+        if t is None:
+            return
         trackdesc = ', '.join([v for v in [ 
             t.trackType, 
-            bind(t.mixer.params.get('Volume'), lambda v: "Vol %4.2f"%v.manual),
-            bind(t.mixer.params.get('Pan'), lambda v: "Pan %3.2f"%v.manual),
+            bind(t.mixer.params.get('Volume'), lambda v: "Vol %4.2f"%v.manual if v is not None and v.manual is not None else None) if t.mixer is not None else None,
+            bind(t.mixer.params.get('Pan'), lambda v: "Pan %3.2f"%v.manual if v is not None and v.manual is not None else None) if t.mixer is not None else None,
         ] if v is not None])
-        print "%d: %s (%s)"%(i, t.name or "<untitled>", trackdesc)
+        print("%d: %s (%s)"%(i, t.name or "<untitled>", trackdesc))
         if show_devices:
             for dev in t.devices:
-                print "  %s"%dev.name
+                print("  %s"%dev.name)
         if show_clips:
             for clip in t.midiclips:
-                print """  Clip "%s" (loop length: %f bars)"""%(clip.name, clip.loopLength or 0) #FIXME
+                print("""  Clip "%s" (loop length: %f bars)"""%(clip.name, clip.loopLength or 0)) #FIXME
+
     if track is not None:
         try:
             track = int(track)
         except ValueError:
             sys.exit("Track option must be a number")
-        if track>len(lsd.tracks):
+        if track > len(lsd.tracks):
             sys.exit("%s has only %d tracks"%(path, len(lsd.tracks)))
-
-        dumptrack(track, lsd.tracks[track-1])
+        if track == 0 and lsd.mastertrack is not None:
+            dumptrack(0, lsd.mastertrack)
+        else:
+            dumptrack(track, lsd.tracks[track-1])
     else:
+        if lsd.mastertrack is not None:
+            dumptrack(0, lsd.mastertrack)
         for (i,t) in enumerate(lsd.tracks):
             dumptrack(i+1, t)
 
